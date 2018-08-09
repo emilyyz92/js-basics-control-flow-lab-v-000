50d159f210d3c4500c91e4075b26e5d6b4ccd047
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-09 14:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(length) {
-  switch(length) {
-    const p1 = "This one is on me!"
-    const p2 = `No can do.`
-    const p3 = 'I will gladly take your thirty bucks.'
-
-    case length <= 199:
-      return p1;
-      break;
-    case length > 2500:
-      return `No can do.`;
-      break;
-    case length > 2000:
-      return 'I will gladly take your thirty bucks.'
-      break;
-  }
+  if (length < 400) {
+    return 'This one is on me!';
+  } else if (length > )
 }
